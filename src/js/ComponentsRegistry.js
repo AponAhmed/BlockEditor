be56dfda09fc3793c515a6aadc1b092ac6123458
MD. Muhiminul Haque <apon2041@gmail.com>
2023-12-09 11:33:12 +0600

    New Component Added
    Column Component add to ComponentRegistry

--- src/js/ComponentsRegistry.js
+++ src/js/ComponentsRegistry.js
@@ -1,7 +1,9 @@
 import Heading from "./Components/Heading.js";
 import Paragraph from "./Components/Paragraph.js";
+import Column from "./Components/Column.js";
 
 const ComponentLists = [
+    { label: "Column", cls: Column },
     { label: "Heading", cls: Heading },
     { label: "Paragraph", cls: Paragraph },
 
